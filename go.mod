b175189fcf63c80f8431bd2b94b2c8330a000740
LeonardoKlaser <leobkklaser@gmail.com>
2025-05-27 08:07:06 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leonardoklaser/Chirpy
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+
+func main() {
+
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:   ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Starting server on :8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
