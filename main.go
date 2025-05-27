62629a955f82daa37b63fb805e18d2b9f03d35b7
LeonardoKlaser <leobkklaser@gmail.com>
2025-05-27 08:37:07 -0300

    add html

--- main.go
+++ main.go
@@ -10,6 +10,12 @@ func main() {
 
 	mux := http.NewServeMux()
 
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fileServer.ServeHTTP(w, r)
+	})
+
 	server := &http.Server{
 		Addr:   ":8080",
 		Handler: mux,
